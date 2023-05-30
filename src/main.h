ade0cec174677e71b425efe5e851f0521a6bcfc5
Sal Bakraa <SalBakraa@gmail.com>
2023-05-30 13:08:17 +0300

    fix!: Avoid clashing with libraries defining a `Point` struct.
    BREAKING: `Point` is renamed to `RectilinearPoint`

--- src/main.h
+++ src/main.h
@@ -5,7 +5,7 @@
 
 typedef struct {
 	int x, y;
-} Point;
+} RectilinearPoint;
 
 typedef struct {
 	unsigned char *data; // Pointer to the RGBA array
@@ -15,7 +15,7 @@ typedef struct {
 } Image;
 
 // The point array is allocated using the "stb_ds.h" library
-const Point *rectilinearize_image(Image *img, size_t *point_count);
-const Point *rectilinearize_file(const char *filename, size_t *point_count);
+const RectilinearPoint *rectilinearize_image(Image *img, size_t *point_count);
+const RectilinearPoint *rectilinearize_file(const char *filename, size_t *point_count);
 
 #endif  // RECTILIEARIZE_H_
